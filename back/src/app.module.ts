217cc02d98ded2a72b6cf6be6fbc8022bff02326
Nethitorn Dew <suradewkiwkong@gmail.com>
2026-06-02 18:02:52 +0700

    structure project

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DonationsModule } from './donations/donations.module';
 
 @Module({
-  imports: [],
+  imports: [DonationsModule],
   controllers: [AppController],
   providers: [AppService],
 })
